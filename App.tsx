34642bfee7901c0bedbe8dac89923b19cb029111
Juan Feris <feris.juan@gmail.com>
2020-11-24 14:17:02 -0500

    Add Point on Long Press

--- App.tsx
+++ App.tsx
@@ -1,12 +1,34 @@
-import { StatusBar } from 'expo-status-bar'
-import React from 'react'
+import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 import { Map, Modal, Panel } from './components'
 
+interface Coordinate {
+	latitude: Number
+	longitude: Number
+}
+
+interface Points {
+	coordinate: Coordinate
+}
+
+interface Point {
+	x: Number
+	y: Number
+}
+
+interface LongPressEvent {
+	nativeEvent: { coordinate: Coordinate; position: Point }
+}
+
 export default function App() {
-	const handleLongPress: any = (nativeEvent: any) => {
-		console.log(nativeEvent)
+	const [points, setPoints] = useState<Points[]>([])
+
+	const handleLongPress: any = ({ nativeEvent }: LongPressEvent) => {
+		const newPoints = points.concat({ coordinate: nativeEvent.coordinate })
+		setPoints(newPoints)
+
+		console.log(points)
 	}
 	return (
 		<View style={styles.container}>
